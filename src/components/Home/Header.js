7e3c989ccbf59bc45262a79f3ab5bd9dd190b338
mattshardman <mattshardman@gmail.com>
2019-03-11 11:22:42 +0000

    add profile card and avatar to header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
@@ -8,22 +9,49 @@ const HeaderContainer = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  height: 60px;
+  height: 65px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 5%;
+  background: #fff;
   box-shadow: 0 3px 35px rgba(0, 0, 0, 0.19);
 `;
 
-function Header({ logOut }) {
-    console.log(logOut)
+const Icons = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Avatar = styled.div`
+  height: 40px;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  overflow: hidden;
+  margin: 0 20px;
+`;
+
+function Header({ logOut, user }) {
+  console.log(user.photoURL)
   return (
     <HeaderContainer>
       <h2>Hi</h2>
+      <Icons>
+      <Avatar>
+        <img src={user.photoURL} alt="" height="100%" />
+      </Avatar>
       <button onClick={logOut}>LOG OUT</button>
+      </Icons>
     </HeaderContainer>
   );
+}
+
+Header.propTypes = {
+  logOut: PropTypes.func.isRequired,
+  user: PropTypes.shape().isRequired,
 }
 
 export default Header;
--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,10 +1,7 @@
 import types from '../constants';
 
 export default (state = false, action) => {
-    console.log(state, action)
     switch (action.type) {
-        case types.LOGGING_IN:
-            return true;
         default: 
             return state;
     }
