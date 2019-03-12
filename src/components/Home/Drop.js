fc050a81fbc8cd855b91ec35a11c793531c2dcd2
mattshardman <mattshardman@gmail.com>
2019-03-12 10:49:58 +0000

    link drop zone with firestore

--- src/components/Home/Drop.js
+++ src/components/Home/Drop.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import styled from "styled-components";
 import { useDropzone } from "react-dropzone";
+import LoadingSpinner from "../LoadingSpinner";
 
 const DropArea = styled.div`
   box-sizing: border-box;
@@ -13,35 +14,60 @@ const DropArea = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 5px;
-  padding: 30px;
   text-align: center;
   cursor: pointer;
 `;
 
+const UploadedImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: ${({ uploadedImage }) => `url("${uploadedImage}")`};
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const DragContent = ({ loading, uploadedImage, isDragActive }) => {
+  if (loading) {
+    return <LoadingSpinner />;
+  } else if (uploadedImage) {
+    return <UploadedImage uploadedImage={uploadedImage} />;
+  } else {
+    if (isDragActive) {
+      return <p>Drop files here</p>;
+    } else {
+      return <p>Drag 'n' drop some files here, or click to select files</p>;
+    }
+  }
+};
+
 function MyDropzone({ storage }) {
+  const [loading, setLoading] = useState(false);
+  const [uploadedImage, setUploadedImage] = useState(null);
   const storageRef = storage.ref();
 
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles[0].name);
     const task = storageRef.child(acceptedFiles[0].name).put(acceptedFiles[0]);
 
+    setLoading(true);
     task
       .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => console.log(url));
+      .then(url => {
+        setLoading(false);
+        setUploadedImage(url);
+      });
   });
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <DropArea
-      {...getRootProps()}
-    >
+    <DropArea {...getRootProps()}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop files here</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      <DragContent
+        loading={loading}
+        uploadedImage={uploadedImage}
+        isDragActive={isDragActive}
+      />
     </DropArea>
   );
 }
