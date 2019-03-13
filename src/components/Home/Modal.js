73079ed1d1ce5b550bc25c0d65f4e171ba0e0f6b
mattshardman <mattshardman@gmail.com>
2019-03-13 11:40:08 +0000

    update modal styling

--- src/components/Home/Modal.js
+++ src/components/Home/Modal.js
@@ -10,7 +10,7 @@ const ModalContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.8);
+  background: rgba(0, 0, 0, 0.9);
   display: flex;
   justify-content: center;
   align-items: center;
