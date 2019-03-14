cf14d5776daeceb6912d5bb51c70de6aea828815
mattshardman <mattshardman@gmail.com>
2019-03-14 08:18:45 +0000

    update favicon and title

--- src/components/AddContent/Drop.js
+++ src/components/AddContent/Drop.js
@@ -1,10 +1,11 @@
 import React, { useState, useCallback } from "react";
 import styled from "styled-components";
 import { useDropzone } from "react-dropzone";
-import LoadingSpinner from "../LoadingSpinner";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const DropArea = styled.div`
   box-sizing: border-box;
+  position: relative;
   margin: 20px 0;
   background: #fff;
   border: 1px solid #eaeaea;
@@ -25,6 +26,14 @@ const DropArea = styled.div`
   }
 `;
 
+const LoadingSection = styled.div`
+  z-index: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
 const UploadedImage = styled.div`
   width: 100%;
   height: 100%;
@@ -36,15 +45,17 @@ const UploadedImage = styled.div`
 `;
 
 const DragContent = ({ loading, uploadedImage, isDragActive }) => {
-  if (loading) {
-    return <LoadingSpinner />;
-  } else if (uploadedImage) {
+  if (uploadedImage) {
     return <UploadedImage uploadedImage={uploadedImage} />;
   } else {
     if (isDragActive) {
-      return <p style={{ margin: 0, padding: '0 50px' }}>Drop file here</p>;
+      return <p style={{ margin: 0, padding: "0 50px" }}>Drop file here</p>;
     } else {
-      return <p style={{ margin: 0, padding: '0 50px' }}>Drag image file here, or click to select files</p>;
+      return (
+        <p style={{ margin: 0, padding: "0 50px" }}>
+          Drag image file here, or click to select files
+        </p>
+      );
     }
   }
 };
@@ -56,16 +67,18 @@ function MyDropzone({ storage, url, setUrl }) {
   const [files, setFiles] = useState([]);
 
   const onDrop = useCallback(acceptedFiles => {
-    setFiles(Object.assign(acceptedFiles[0], {
-      preview: URL.createObjectURL(acceptedFiles[0])
-    }));
-    setUrl(files.preview)
+    setFiles(
+      Object.assign(acceptedFiles[0], {
+        preview: URL.createObjectURL(acceptedFiles[0])
+      })
+    );
+    setLoading(true);
+    setUrl(files.preview);
     const task = storageRef.child(acceptedFiles[0].name).put(acceptedFiles[0]);
-
     task
       .then(snapshot => {
-        console.log(snapshot)
-        return snapshot.ref.getDownloadURL()
+        console.log(snapshot);
+        return snapshot.ref.getDownloadURL();
       })
       .then(url => {
         setLoading(false);
@@ -74,10 +87,13 @@ function MyDropzone({ storage, url, setUrl }) {
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-  console.log(files)
-
   return (
     <DropArea {...getRootProps()}>
+      {loading && (
+        <LoadingSection>
+          <LinearProgress />
+        </LoadingSection>
+      )}
       <input {...getInputProps()} />
       <DragContent
         loading={loading}
